d4a91714897740d49fe087ace6db87baa6004fe0
Maxmaus301 <vmaxgel@mail.ru>
2023-10-09 20:11:56 +0300

    Добавлена функция set_expense_table() для заполнения таблицы расходов

--- bookkeeper/view/exp_widget.py
+++ bookkeeper/view/exp_widget.py
@@ -1,6 +1,6 @@
-import sys
+from typing import Any
 from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QTableWidget, QHeaderView,
-QAbstractItemView)
+QAbstractItemView, QTableWidgetItem)
 from PySide6.QtCore import Qt, QSize
 
 
@@ -10,6 +10,7 @@ class ExpenseWidget(QMainWindow):
     """
     def __init__(self):
         super(ExpenseWidget, self).__init__()
+        self.data: list[Any] = []
 
         name_horizontal_label = ['Дата', 'Сумма', 'Категория', 'Комментарий'] # Список имен столбцов таблицы расходов
 
@@ -20,7 +21,8 @@ class ExpenseWidget(QMainWindow):
         label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
         layout.addWidget(label)
 
-        self.table_exp_widget = QTableWidget(0, 4)
+        self.table_exp_widget = QTableWidget()
+        self.table_exp_widget.setColumnCount(4)
 
 
         header = self.table_exp_widget.horizontalHeader()
@@ -29,13 +31,33 @@ class ExpenseWidget(QMainWindow):
         header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
         header.setSectionResizeMode(3, QHeaderView.ResizeMode.Stretch)
 
-
         self.table_exp_widget.setHorizontalHeaderLabels(name_horizontal_label)
 
         layout.addWidget(self.table_exp_widget)
 
-
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+    def set_expense_table(self, data, size):
+        """ Заполнить таблицу расходов """
+        name_horizontal_label = ['Дата', 'Сумма', 'Категория', 'Комментарий']  # Список имен столбцов таблицы расходов
+        self.data = data
+        self.data.sort(key=lambda d: d["expense_date"], reverse=True)
+        self.table_exp_widget.clear()
+        self.table_exp_widget.setRowCount(size)
+        for i, row in enumerate(self.data):
+            item = QTableWidgetItem(row["expense_date"])
+            self.table_exp_widget.setItem(i, 0, item)
+
+            item = QTableWidgetItem(str(row["amount"]))
+            self.table_exp_widget.setItem(i, 1, item)
+
+            item = QTableWidgetItem(row["category"].capitalize())
+            self.table_exp_widget.setItem(i, 2, item)
+
+            item = QTableWidgetItem(row["comment"])
+            self.table_exp_widget.setItem(i, 3, item)
+
+        self.table_exp_widget.setHorizontalHeaderLabels(name_horizontal_label)
 
